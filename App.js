0870f563ac2a81dc7a2320c93d101cded2122ba3
shoulderkarlsson <shoulderkarlsson@gmail.com>
2017-12-09 20:35:46 +0100

    misc: prettier

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-import React from "react"
-import {StyleSheet, Text, View, Button, AppRegistry} from "react-native"
-import {Cube} from "./components/Cube"
-import * as colors from "./lib/colors"
-import {MenuText} from "./components/MenuText"
-import {generateRandomColors} from "./lib/randomColors"
-import {NativeRouter, Link} from "react-router-native"
-import {Navigation, Card} from "react-router-navigation"
-import {Main} from "./pages/Main"
+import React from 'react'
+import {StyleSheet, Text, View, Button, AppRegistry} from 'react-native'
+import {Cube} from './components/Cube'
+import * as colors from './lib/colors'
+import {MenuText} from './components/MenuText'
+import {generateRandomColors} from './lib/randomColors'
+import {NativeRouter, Link} from 'react-router-native'
+import {Navigation, Card} from 'react-router-navigation'
+import {Main} from './pages/Main'
+import {Algorithms} from './pages/Algorithms'
 
 export default () => (
   <NativeRouter>
     <Navigation>
-      <Card exact path='/algorithms' render={() => <Text>placeholer</Text>} />
+      <Card exact path="/algorithms" render={Algorithms} />
       <Card exact path="/" render={Main} />
     </Navigation>
   </NativeRouter>
