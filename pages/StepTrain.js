c0f81049fcbc837d27d684b9a158b3d2f9d21bb6
shoulderkarlsson <shoulderkarlsson@gmail.com>
2017-12-12 23:56:56 +0100

    feat: refactored component and almost done with calculations if algorithm is in correct order

--- pages/StepTrain.js
+++ pages/StepTrain.js
@@ -6,33 +6,75 @@ import {Header} from '../components/common/Header'
 import {Square} from '../components/common/Square'
 
 const styles = StyleSheet.create({
-  guessesContainer: {},
-  guessOptionsContainer: {},
+  squareContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 24,
+  },
+
+  scoreContainer: {},
 })
 
 class StepTrain extends Component {
   state = {
+    amountOfErrors: 0,
+    amountOfCorrect: 0,
     guesses: [],
+    correctSequence: [],
     stage: {},
   }
 
   componentDidMount() {
     const {stageId} = this.props.match.params
     const stage = stages.find(({stageNumber}) => stageId == stageNumber)
-    this.setState({stage: {...stage}})
-  }
-
-  onGuess = (guess, pos) => {
     this.setState({
-      guesses: [...this.state.guesses, {guess, pos}],
+      stage: {...stage},
+      correctSequence: [...stage.algs[0].steps],
     })
   }
 
-  render() {
-    if (!Object.keys(this.state.stage).length) {
-      return null
-    }
+  evaluateGuess = (guess, position) => {
+    this.setState(() => ({
+      guesses: [...this.state.guesses, {guess, pos: position}],
+    }))
+    this.foo()
+  }
 
+  foo = () => {
+    const {amountOfCorrect, amountOfErrors} = this.state.guesses.reduce(
+      (curr, {guess}, i) => {
+        if (this.state.correctSequence[i] === guess) {
+          return {
+            ...curr,
+            amountOfCorrect: curr.amountOfCorrect + 1,
+          }
+        } else {
+          return {
+            ...curr,
+            amountOfErrors: curr.amountOfErrors + 1,
+          }
+        }
+      }, {amountOfCorrect: 0, amountOfErrors: 0}
+    )
+
+    this.setState(() => ({
+      amountOfCorrect, amountOfErrors
+    }))
+
+    console.log('amountOfCorrect :', amountOfCorrect)
+    console.log('amountOfErrors :', amountOfErrors)
+  }
+
+  render() {
+    // TODO: refactor this
+    // This is because in componentDidMount we are collecting the stage
+    // maybe provide this to component as prop or something like that...
+    if (!Object.keys(this.state.stage).length) return null
+
+    const {guesses} = this.state
     const [alg] = this.state.stage.algs
     return (
       <View style={globalStyles.container}>
@@ -42,77 +84,26 @@ class StepTrain extends Component {
           fontSize={32}
         />
         <Header text={this.state.stage.title} color="#e0e0e0" fontSize={16} />
-        <View
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            marginTop: 12,
-          }}
-        >
-          {alg.steps.map((_, i) => {
-            const guess = this.state.guesses[i]
-              ? this.state.guesses[i].guess
-              : null
-            return <Square key={i} text={guess} />
-            // return (
-            //   <View
-            //     key={i}
-            //     style={{
-            //       height: 64,
-            //       width: 64,
-            //       backgroundColor: 'rgba(1,1,1, 0.4)',
-            //       display: 'flex',
-            //       justifyContent: 'center',
-            //       alignItems: 'center',
-            //       margin: 6,
-            //     }}
-            //   >
-            //     {this.state.guesses[i] ? (
-            //       <Text style={{color: '#fff', fontSize: 64}}>
-            //         {this.state.guesses[i].guess}
-            //       </Text>
-            //     ) : null}
-            //   </View>
-            // )
+        <View style={styles.squareContainer}>
+          {alg.steps.map((_, i) => (
+            <Square key={i} text={guesses[i] ? guesses[i].guess : null} />
+          ))}
+        </View>
+        <View style={styles.squareContainer}>
+          {alg.steps.map((step, i) => {
+            const hasMadeGuess = guesses.find(({pos}) => pos === i)
+            return (
+              <Square
+                key={i}
+                text={!hasMadeGuess ? step : null}
+                onClick={() => this.evaluateGuess(step, i)}
+              />
+            )
           })}
         </View>
-        <View
-          style={{
-            marginTop: 32,
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexWrap: 'wrap',
-            flexDirection: 'row',
-          }}
-        >
-          {alg.steps.map((step, i) => (
-            <View
-              key={i}
-              style={{
-                height: 70,
-                width: 70,
-                padding: 2,
-                margin: 6,
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: 'rgba(1,1,1, 0.4)',
-              }}
-            >
-              {this.state.guesses.find(({pos}) => pos === i) ? null : (
-                <Text
-                  style={{color: '#fff', fontSize: 64}}
-                  onPress={() => this.onGuess(step, i)}
-                >
-                  {step}
-                </Text>
-              )}
-            </View>
-          ))}
+        <View style={styles.scoreContainer}>
+          <Text>{`Correct ${this.state.amountOfCorrect}`}</Text>
+          <Text>{`Wrong: ${this.state.amountOfErrors}`}</Text>
         </View>
       </View>
     )
