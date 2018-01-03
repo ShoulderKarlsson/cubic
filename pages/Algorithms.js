872bab962f8ccc96cf930463c5bdb31e3295005c
shoulderkarlsson <shoulderkarlsson@gmail.com>
2018-01-03 23:05:15 +0100

    feat: impl. started on refactoring presentation of the different stages

--- pages/Algorithms.js
+++ pages/Algorithms.js
@@ -1,8 +1,13 @@
 import React from 'react'
-import {Text, View, StyleSheet} from 'react-native'
+import {Text, View, StyleSheet, ScrollView} from 'react-native'
 import {globalStyles} from '../lib/common-styles'
 import {stages} from '../lib/stages'
 import {NavLink} from '../components/NavLink'
+import {Square} from '../components/common/Square'
+import {Header} from '../components/common/Header'
+import {RED} from '../lib/colors'
+import Dimensions from 'Dimensions'
+const win = Dimensions.get('window')
 
 const styles = StyleSheet.create({
   stageLinkContainer: {
@@ -20,29 +25,70 @@ const styles = StyleSheet.create({
   },
 })
 
-export const Algorithms = ({...props}) => {
+export const Algorithms = () => {
   return (
-    <View style={globalStyles.container}>
-      {stages.map(({stageNumber, title, algs, ...props}, i) => {
+    <ScrollView
+      contentContainerStyle={{
+        backgroundColor: RED,
+      }}
+    >
+      <View
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'flex-end',
+          marginRight: 16,
+          marginTop: 16,
+        }}
+      >
+        <Square
+          squareHeight={48}
+          squareWidth={48}
+          textFontSize={24}
+          onClick={() => {}}
+          text={'?'}
+        />
+      </View>
+      {stages.map(({title, stageNumber, note}, i) => {
         return (
-          <View style={styles.stageLinkContainer} key={i}>
-            <View style={styles.topSection}>
-              <NavLink
-                to={`/algorithms/${stageNumber}`}
-                text={`Step ${stageNumber}`}
-              />
-              <Text
-                style={{
-                  color: '#e0e0e0',
-                  fontSize: 16,
-                }}
-              >
-                {title}
-              </Text>
+          <View
+            key={i}
+            style={{
+              height: win.height / 2,
+              marginLeft: 16,
+              marginRight: 16,
+              backgroundColor: 'rgba(1,1,1, 0.4)',
+              marginBottom: 8,
+            }}
+          >
+            <View
+              style={{
+                flex: 2,
+                display: 'flex',
+              }}
+            >
+              <View style={[globalStyles.centeredContent, {flex: 1}]}>
+                <NavLink
+                  to={`/algorithms/${stageNumber}`}
+                  text={title}
+                  style={{fontSize: 64}}
+                />
+              </View>
+              <View style={[globalStyles.centeredContent, {flex: 1}]}>
+                <Text
+                  style={{
+                    color: '#fff',
+                    fontSize: 16,
+                    fontWeight: 'bold',
+                  }}
+                >
+                  {note}
+                </Text>
+              </View>
             </View>
           </View>
         )
       })}
-    </View>
+    </ScrollView>
   )
 }
