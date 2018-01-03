b0d6deb305732847d07c97bf99f4f4773e2e0e3a
shoulderkarlsson <shoulderkarlsson@gmail.com>
2018-01-03 20:49:39 +0100

    fix: added more styles configuration

--- components/common/Square.js
+++ components/common/Square.js
@@ -18,14 +18,39 @@ const styles = StyleSheet.create({
   },
 })
 
+const getStyles = ({
+  squareHeight,
+  squareWidth,
+  squareBackgroundColor,
+  textFontSize,
+  textColor,
+}) => ({
+  square: {
+    height: squareHeight ? squareHeight : 72,
+    width: squareWidth ? squareWidth : 72,
+    backgroundColor: squareBackgroundColor
+      ? squareBackgroundColor
+      : 'rgba(1,1,1, 0.4)',
+    display: 'flex',
+    justifyContent: 'center',
+    margin: 6,
+  },
 
+  textStyle: {
+    fontSize: textFontSize ? textFontSize : 64,
+    color: textColor ? textColor : '#fff',
+  },
+})
 
-export const Square = ({text, onClick = () => {}}) => (
-  <View style={styles.square}>
-    {!!text ? (
-      <Text style={styles.textStyle} onPress={() => onClick()}>
-        {text}
-      </Text>
-    ) : null}
-  </View>
-)
+export const Square = ({text, onClick = () => {}, ...props}) => {
+  const {square, textStyle} = getStyles(props)
+  return (
+    <View style={styles.square}>
+      {!!text ? (
+        <Text style={styles.textStyle} onPress={() => onClick()}>
+          {text}
+        </Text>
+      ) : null}
+    </View>
+  )
+}
