1c9e378d301c4379cd01fc464c97fc0b1eb20525
shoulderkarlsson <shoulderkarlsson@gmail.com>
2018-01-05 21:56:03 +0100

    fix: favoring Square component over Cube

--- pages/Main.js
+++ pages/Main.js
@@ -2,20 +2,13 @@ import React from 'react'
 import {StyleSheet, Text, View} from 'react-native'
 import {NavLink} from '../components/NavLink'
 import {Cube} from '../components/Cube'
+import {Square} from '../components/common/Square'
 import {generateRandomColors} from '../lib/common-helpers'
 import {RED} from '../lib/colors'
+import {globalStyles} from '../lib/common-styles'
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: RED,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   cubes: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
     flexDirection: 'row',
     width: 204,
     height: 204,
@@ -30,17 +23,21 @@ const styles = StyleSheet.create({
   },
 })
 
-export const Main = ({match}) => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.cubes}>
-        {generateRandomColors(9).map((color, i) => (
-          <Cube key={i} color={color} />
-        ))}
-      </View>
-      <View style={styles.optionsContainer}>
-        <NavLink to={'/algorithms'} text="Start Learning!" />
-      </View>
+export const Main = ({match}) => (
+  <View style={[globalStyles.centeredContent, {flex: 1, backgroundColor: RED}]}>
+    <View style={[globalStyles.centeredContent, styles.cubes]}>
+      {generateRandomColors(9).map((color, i) => (
+        <Square
+          key={i}
+          squareBackgroundColor={color}
+          squareMargin={2}
+          squareHeight={64}
+          squareWidth={64}
+        />
+      ))}
     </View>
-  )
-}
+    <View style={styles.optionsContainer}>
+      <NavLink to={'/algorithms'} text="Start Learning!" />
+    </View>
+  </View>
+)
