af1bff47b8040fd5f419d6e394c46364a195038c
shoulerkarlsson <shoulderkarlsson@gmail.com>
2018-04-27 23:13:53 +0200

    feat: added endpoint and created basic component skeleton

--- recube/src/ui/navigation/android-nav.js
+++ recube/src/ui/navigation/android-nav.js
@@ -2,9 +2,14 @@ import React from 'react'
 import {StackNavigator} from 'react-navigation'
 import {View, Text} from 'react-native'
 import {AndroidMain} from '../pages/android-main'
+import {Train} from '../pages/train'
 
 export const AndroidNavigation = StackNavigator({
   Home: {
-    screen: AndroidMain
+    screen: AndroidMain,
+  },
+
+  Train: {
+    screen: Train,
   },
 })
---
+++ recube/src/ui/pages/train.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View, Text, StyleSheet, TouchableNativeFeedback} from 'react-native'
+
+
+export const Train = ({...props}) => {
+  return (
+    <View>
+      <Text>
+        Train stuff here.
+      </Text>
+    </View>
+  )
+}
