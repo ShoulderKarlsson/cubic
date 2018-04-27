9d6b9ab5c0fcbbdcb121cb00c89cbe329463bd80
shoulerkarlsson <shoulderkarlsson@gmail.com>
2018-04-28 01:29:54 +0200

    feat: rendering cubes with cube component

--- recube/src/ui/pages/train.js
+++ recube/src/ui/pages/train.js
@@ -9,14 +9,36 @@ import {
 } from 'react-native'
 import {stages} from '../../lib/stages'
 import {globalStyles} from '../../lib/common-styles'
+import {WHITE, RED} from '../../lib/colors'
+import {Cube} from '../components/cube'
 const windowDimensions = Dimensions.get('window')
 
 const styles = StyleSheet.create({
   stepContainer: {
     height: windowDimensions.height,
     width: windowDimensions.width,
-    backgroundColor: 'orange',
+    // backgroundColor: 'orange',
     margin: 10,
+  },
+
+  titleAndDescription: {
+    flex: 1.2,
+    justifyContent: 'space-around',
+    marginLeft: 3,
+    marginRight: 3,
+  },
+
+  title: {
+    fontSize: 24,
+    color: WHITE,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  description: {
+    fontSize: 14,
+    color: '#DDDDDD', // TODO => Make this into a better color
+    textAlignVertical: 'center',
+    textAlign: 'center',
   },
 })
 
@@ -27,14 +49,68 @@ const StatelessTrain = ({...props}) => {
         alignItems: 'center',
         justifyContent: 'center',
         margin: 3,
+        backgroundColor: RED,
       }}
     >
-      {stages.map((stage, i) => {
-        return (
-          <View style={styles.stepContainer} key={i}>
-          </View>
-        )
-      })}
+      {stages.map(
+        (
+          {title, description, cubeStates = {exampleStart: [], finished: []}},
+          i
+        ) => {
+          console.log(cubeStates)
+          return (
+            <View style={styles.stepContainer} key={i}>
+              {/* Top container (Includes title and description) */}
+              <View style={[globalStyles.XYCenter, styles.titleAndDescription]}>
+                <Text style={styles.title}>{title}</Text>
+                <Text style={[styles.description]}>{description}</Text>
+              </View>
+
+              {/* Middle section, (Cube with results) */}
+              <View
+                style={{
+                  flex: 2,
+                  flexDirection: 'row',
+                }}
+              >
+                <View
+                  style={[
+                    {flex: 1, backgroundColor: 'purple'},
+                    globalStyles.XYCenter,
+                  ]}
+                >
+                  <Cube
+                    cubeColors={cubeStates.exampleStart}
+                    brickSize={60}
+                    brickMargin={2}
+                  />
+                </View>
+
+                <View
+                  style={[
+                    {flex: 1, backgroundColor: 'yellow'},
+                    globalStyles.XYCenter,
+                  ]}
+                >
+                  <Cube
+                    cubeColors={cubeStates.finished}
+                    brickSize={60}
+                    brickMargin={2}
+                  />
+                </View>
+              </View>
+
+              {/* Bottom Section (Button to step-train thingy) */}
+              <View
+                style={{
+                  flex: 0.8,
+                  backgroundColor: 'green',
+                }}
+              />
+            </View>
+          )
+        }
+      )}
     </ScrollView>
   )
 }
