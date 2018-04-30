0527e33eb8721e878fd12977f2752ec58326cf34
shoulerkarlsson <shoulderkarlsson@gmail.com>
2018-04-30 06:21:02 +0200

    fix: added size determination helper function

--- cubic/src/ui/components/cbutton.js
+++ cubic/src/ui/components/cbutton.js
@@ -3,8 +3,14 @@ import {View, TouchableNativeFeedback, Text} from 'react-native'
 import propTypes from 'prop-types'
 import {WHITE} from '../../lib/colors'
 
+const getFontSizeFor = size => {
+  if (size === 'small') return 15
+  if (size === 'medium') return 24
+  if (size === 'large') return 32
+}
 export const CButton = ({label, onPress, size}) => {
-  
+  const fontSize = getFontSizeFor(size.toLowerCase())
+
   return (
     <TouchableNativeFeedback
       onPress={onPress}
@@ -16,14 +22,7 @@ export const CButton = ({label, onPress, size}) => {
           backgroundColor: 'rgba(104, 65, 65, 0.45)',
         }}
       >
-        <Text
-          style={{
-            color: WHITE,
-            fontSize: 32
-          }}
-        >
-          {label}
-        </Text>
+        <Text style={{color: WHITE, fontSize}}>{label}</Text>
       </View>
     </TouchableNativeFeedback>
   )
